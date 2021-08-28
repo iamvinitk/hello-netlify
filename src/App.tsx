092e5e0754b345be7ea5b68e7315c5c5292adf22
Vinit Kanani <Vinit.Kanani@cimpress.com>
2021-08-28 13:51:07 +0530

    Second  commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1 style={{fontFamily: 'Nunito, sans-serif;', textAlign: 'center', fontSize: 104}}>Hello World,</h1>
-    </div>
-  );
+const TYPING_SPEED = 150;
+const DELETING_SPEED = 60;
+
+const dataText = ['JavaScript.', 'Python.', "React.", "MongoDB."]
+
+export default class App extends React.Component {
+
+  state = {
+    text: '',
+    isDeleting: false,
+    loopNum: 0,
+    typingSpeed: TYPING_SPEED
+  }
+
+  componentDidMount() {
+    this.handleType();
+  }
+
+  handleType = () => {
+    const { isDeleting, loopNum, text, typingSpeed } = this.state;
+    const i = loopNum % dataText.length;
+    const fullText = dataText[i];
+
+    this.setState({
+      text: isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1),
+      typingSpeed: isDeleting ? DELETING_SPEED : TYPING_SPEED
+    });
+
+    if (!isDeleting && text === fullText) {
+      setTimeout(() => this.setState({ isDeleting: true }), 500);
+    } else if (isDeleting && text === '') {
+      this.setState({
+        isDeleting: false,
+        loopNum: loopNum + 1
+      });
+    }
+
+    setTimeout(this.handleType, typingSpeed);
+  };
+
+  render() {
+    return (
+        <h1 style={{textAlign: 'center', fontFamily: 'Nunito', fontSize: 104}}>Hello World,
+          <br/>
+          I do &nbsp;
+          <span>{ this.state.text }</span>
+          <span id="cursor"/>
+        </h1>
+    );
+  }
 }
 
-export default App;
